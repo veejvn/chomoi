6c7d14203c2f6cd71a1ad8bf75f561e8d37ca6dd
Yogurt <nguyenday3332@gmail.com>
2024-10-01 08:03:22 +0700

    first commit

---
+++ src/test/java/com/ecommerce/chomoi/ChomoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.chomoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChomoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
